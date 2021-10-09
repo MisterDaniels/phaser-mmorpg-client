210c0b76cc8cf3ed44de108db5bfa8bfe5e9592e
Daniel Silva <dodosilva51@gmail.com>
2021-10-09 11:42:26 -0300

    Created contants file
    Now the client is running with .js builder and http server

--- js/game.js
+++ js/game.js
@@ -1,13 +1,33 @@
 import Phaser from 'phaser';
+import constants from './constants.json';
 
 // create new scene
 let scene = new Phaser.Scene('Game');
 
+// load assets
+scene.preload = function() {
+    // load images
+    this.load.image('background', 'assets/background.png');
+    this.load.image('player', 'assets/player.png');
+};
+
+// called once after the preload ends
+scene.create = function() {
+    // create bg sprite
+    let background = this.add.sprite(0, 0, 'background');
+
+    // change the origin to the top-left corner
+    background.setOrigin(0, 0);
+
+    let player = this.add.sprite(0, 0, 'player');
+    player.setPosition(constants.screen.width / 2, constants.screen.height / 2);
+};
+
 // set the configuration of the game
 const config = {
     type: Phaser.AUTO,
-    width: 640,
-    height: 360,
+    width: constants.screen.width,
+    height: constants.screen.height,
     scene: scene
 };
 
